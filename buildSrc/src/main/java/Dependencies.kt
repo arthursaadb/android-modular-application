2e1ecc57fb880b28d354d8e9b37a6a3ef9c1a7df
Saad <arthursaadb@gmail.com>
2021-02-21 20:23:00 -0300

    First commit and move gradle to kotlin dsl

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,39 @@
+object Dependencies {
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val MATERIAL = "com.google.android.material:material:${Versions.material}"
+    const val CONSTRAINT_LAYOUT =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val CORE_KTX = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val COLLECTION_KTX = "androidx.collection:collection-ktx:${Versions.collectionKtx}"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.fragmentKtx}"
+    const val LIFECYCLE_KTX = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleKtx}"
+    const val LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.liveDataKtx}"
+    const val NAVIGATION_KTX =
+        "androidx.navigation:navigation-runtime-ktx:${Versions.navigationKtx}"
+    const val NAVIGATION_FRAGMENT_KTX =
+        "androidx.navigation:navigation-fragment-ktx:${Versions.navigationKtx}"
+    const val NAVIGATION_UI_KTX = "androidx.navigation:navigation-ui-ktx:${Versions.navigationKtx}"
+    const val ROOM_KTX = "androidx.room:room-ktx:${Versions.roomKtx}"
+    const val VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.viewModelKtx}"
+    const val NAVIGATION_FRAGMENT =
+        "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+    const val NAVIGATION_MODULE =
+        "androidx.navigation:navigation-dynamic-features-fragment:${Versions.navigation}"
+    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+    const val PAGING = "androidx.paging:paging-runtime:${Versions.pagination}"
+    const val ROOM_RUNTIME = "androidx.room:room-runtime:${Versions.room}"
+    const val ROOM_COMPILER = "androidx.room:room-compiler:${Versions.room}"
+    const val HILT_PLUG_IN = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}"
+    const val HILT = "com.google.dagger:hilt-android:${Versions.hilt}"
+    const val HILT_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+    const val COIL = "io.coil-kt:coil:${Versions.coil}"
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val RETROFIT_CONVERTER = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+    const val MOSHI = "com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshi}"
+    const val MOSHI_KTX = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
+    const val JUNIT = "junit:junit:${Versions.junit}"
+    const val EXTJUNIT = "androidx.test.ext:junit:${Versions.extJunit}"
+    const val EXPRESSO = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,49 @@
+object Versions {
+    // app level
+    const val gradle = "4.1.2"
+    const val kotlin = "1.4.21"
+
+    // ui
+    const val appcompat = "1.2.0"
+    const val material = "1.3.0"
+    const val constraintLayout = "2.0.4"
+
+    // ktx
+    const val coreKtx = "1.3.2"
+    const val collectionKtx = "1.1.0"
+    const val fragmentKtx = "1.2.5"
+    const val lifecycleKtx = "2.3.0"
+    const val liveDataKtx = "2.3.0"
+    const val navigationKtx = "2.3.3"
+    const val roomKtx = "2.2.6"
+    const val viewModelKtx = "2.3.0"
+
+    // navigation
+    const val navigation = "2.3.3"
+
+    // coroutines
+    const val coroutines = "1.3.9"
+
+    // pagination
+    const val pagination = "2.1.2"
+
+    // room
+    const val room = "2.2.6"
+
+    // hilt
+    const val hilt = "2.28-alpha"
+
+    // coil
+    const val coil = "1.1.1"
+
+    // retrofit
+    const val retrofit = "2.9.0"
+
+    // moshi
+    const val moshi = "1.11.0"
+
+    // test
+    const val junit = "4.13.2"
+    const val extJunit = "1.1.1"
+    const val espresso = "3.3.0"
+}
